82f4c0ddd18326386fa0d42472d0ebaafaf57036
Lucas <loukaka41@gmail.com>
2023-07-25 14:41:10 +0200

    :lipstick: style change in heading

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -16,7 +16,7 @@ const Heading: React.FC<HeadingProps> = ({
             <div className="sm:text-2xl text-xl font-bold">
                 {title}
             </div>
-            <div className="font-light text-neutral-500 mt-2">
+            <div className="font-light text-neutral-500 mt-2 mb-3">
                 {subtitle}
             </div>
             <hr className="w-5/6 m-auto"/>
